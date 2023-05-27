84786b21ae16f8f9192b93e4d65c7555f39a816f
André Ferreira <andreferreira6578@gmail.com>
2023-05-27 12:06:33 +0100

    Removed useless validation for pokemonList

--- pokedex/src/Pokemon.jsx
+++ pokedex/src/Pokemon.jsx
@@ -18,15 +18,13 @@ export default function App() {
 
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=150")
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=150`)
       .then((response) => {
         const pokemons = response.data.results;
         const fetchData = async () => {
           const promises = pokemons.map((pokemon) =>
             axios.get(pokemon.url).then((response) => response.data)
           );
-
-          console.log(promises);
 
           const resolvedPokemons = await Promise.all(promises);
           const sortedPokemons = resolvedPokemons.sort((a, b) => a.id - b.id);
@@ -45,78 +43,71 @@ export default function App() {
     setFilteredPokemonList(filteredList);
   }, [searchTerm, pokemonList]);
 
-  if (pokemonList.length > 0) {
-    console.log(pokemonList);
-    return renderPokemonCards();
-  }
-
-  function renderPokemonCards() {
-    return (
-      <>
-        <Container className={"custom-container"}>
-          <input
-            className="custom-input-name"
-            type="text"
-            placeholder="Search by name"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <Row className="custom-row">
-            {filteredPokemonList.map(({ id, name, sprites, types }) => (
-              <Col className="custom-col" sm={6} md={4} lg={4} key={id}>
-                <Card className="mx-auto" sx={{ width: "48%" }}>
-                  <img
-                    className="pokemon-img"
-                    src={
-                      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-                      sprites.other.home.front_default
-                        .split("sprites/pokemon/")[1]
-                        .slice(0, -4) +
-                      ".png"
-                    }
-                    id={id}
-                  />
-                  <CardContent>
-                    <Typography
-                      className="pokemon-name"
-                      variant="h5"
-                      component="div"
-                    >
-                      {name}
-                    </Typography>
-                    <div className="pokemon-types text-center">
-                      {types.map(({ type }) => (
-                        <Typography
-                          className={type.name}
-                          sx={{
-                            fontSize: "12px",
-                            display: "flex",
-                            width: "70px",
-                            height: "20px",
-                            alignItems: "center",
-                            borderRadius: "5px",
-                            margin: "0 auto",
-                            justifyContent: "center",
-                          }}
-                          variant="h5"
-                          component="div"
-                        >
-                          {type.name}
-                        </Typography>
-                      ))}
-                    </div>
-                  </CardContent>
-                  <CardActions>
-                    <Button href={"/pokemon/" + id} size="small">
-                      See more...
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <Container className={"custom-container"}>
+        <input
+          className="custom-input-name"
+          type="text"
+          placeholder="Search by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Row className="custom-row">
+          {filteredPokemonList.map(({ id, name, sprites, types }) => (
+            <Col className="custom-col" sm={6} md={4} lg={4} key={id}>
+              <Card className="mx-auto" sx={{ width: "48%" }}>
+                <img
+                  className="pokemon-img"
+                  src={
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+                    sprites.other.home.front_default
+                      .split("sprites/pokemon/")[1]
+                      .slice(0, -4) +
+                    ".png"
+                  }
+                  id={id}
+                />
+                <CardContent>
+                  <Typography
+                    className="pokemon-name"
+                    variant="h5"
+                    component="div"
+                  >
+                    {name}
+                  </Typography>
+                  <div className="pokemon-types text-center">
+                    {types.map(({ type }) => (
+                      <Typography
+                        className={type.name}
+                        sx={{
+                          fontSize: "12px",
+                          display: "flex",
+                          width: "70px",
+                          height: "20px",
+                          alignItems: "center",
+                          borderRadius: "5px",
+                          margin: "0 auto",
+                          justifyContent: "center",
+                        }}
+                        variant="h5"
+                        component="div"
+                      >
+                        {type.name}
+                      </Typography>
+                    ))}
+                  </div>
+                </CardContent>
+                <CardActions>
+                  <Button href={"/pokemon/" + id} size="small">
+                    See more...
+                  </Button>
+                </CardActions>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
